6b9723d015e7415da620af63e6f1fd5db58e8cbf
Shriharsh <shriharshgautam7@gmail.com>
2023-05-15 22:17:31 +0530

    Added header for different routes

--- another_server/index.js
+++ another_server/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 
 const hostname = '127.0.0.1';
-const port = 4017;
+const port = 4018;
 const home = fs.readFileSync('./index.html')
 const about = fs.readFileSync('./about.html')
 const services = fs.readFileSync('./services.html')
