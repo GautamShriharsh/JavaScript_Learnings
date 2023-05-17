f3854110907b56c13b8946e62aac45e96ab9eb33
Shriharsh <shriharshgautam7@gmail.com>
2023-05-17 21:36:55 +0530

    Added a /about route and another /about route for post request

--- express/app.js
+++ express/app.js
@@ -1,10 +1,18 @@
-const express = require("express")
+const express = require("express");
 
 const app = express();
 const port = 80;
 
 app.get('/', (req,res)=>{
-    res.send("This is my first express app")
+    res.send("This is my first express app");
+})
+
+app.get('/about', (req,res)=>{
+    res.send("This is the about page of my express app");
+})
+
+app.post('/about', (req,res)=>{
+    res.send("This is the post req about page of my first express app");
 })
 
 app.listen(port, ()=>{
