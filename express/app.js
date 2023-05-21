5fe19a174c0319f7f3335dcce7b02c601a69c7d5
Shriharsh <shriharshgautam7@gmail.com>
2023-05-21 12:28:08 +0530

    Modified app.js

--- express/app.js
+++ express/app.js
@@ -1,20 +1,23 @@
 const express = require("express");
-
+const path = require("path");
+const fs = require("fs");
 const app = express();
 const port = 80;
 
-app.get('/', (req,res)=>{
-    res.send("This is my first express app");
+app.use('/static', express.static('static')) // For serving static files
+
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+
+
+
+app.get('/', (req, res)=>{
+    const con = "This is the best content on the internet so far so use it wisely"
+    const params = {'title': 'PubG is the best game', "content": con}
+    res.status(200).render('index.pug', params);
 })
 
-app.get('/about', (req,res)=>{
-    res.send("This is the about page of my express app");
-})
-
-app.post('/about', (req,res)=>{
-    res.send("This is the post req about page of my first express app");
-})
-
+// START THE SERVER
 app.listen(port, ()=>{
-    console.log(`The application has started successfully on ${port}`);
-} )
+    console.log(`The application started successfully on port ${port}`);
+});
