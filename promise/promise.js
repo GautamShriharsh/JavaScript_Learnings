63c9d40a1f6b8b54a7293e98f0f91d898d410500
Shriharsh <shriharshgautam7@gmail.com>
2023-12-24 13:53:59 +0530

    created promise Four

--- promise/promise.js
+++ promise/promise.js
@@ -29,5 +29,29 @@ const promiseThree = new Promise(function(resolve, reject){
 })
 
 promiseThree.then(function(user){
- console.log(user.username);
+ console.log(user);
 })
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = false
+        if (!error) {
+            resolve({username: 'Shriharsh', password: '345'})
+        } else {
+            reject('ERROR: Something went wrong')
+        }
+
+    }, 1000)
+})
+
+promiseFour
+.then(function(user){
+    console.log(user);
+    return user.username
+}).then((username) => {
+  console.log(username);
+}).catch(function(error){
+    console.log(error);
+}).finally(() => console.log('The promise is either resolved or rejected'))
+
+
