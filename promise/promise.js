655731375895c789ebd954683a550e33b34c52ea
Shriharsh <shriharshgautam7@gmail.com>
2023-12-25 19:38:08 +0530

    created fetch function

--- promise/promise.js
+++ promise/promise.js
@@ -79,15 +79,25 @@
 // consumePromiseFive()
 
 
-async function getAllUsers(){
-try {
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
-    const data =  await response.json()
+// async function getAllUsers(){
+// try {
+//     const response = await fetch('https://jsonplaceholder.typicode.com/users')
+//     const data =  await response.json()
+//     console.log(data);
+// } catch (error) {
+//     console.log("Error: ", error);
+// }
+// }
+
+// getAllUsers()
+
+
+fetch('https://jsonplaceholder.typicode.com/users').then((response) => {
+return response.json()
+})
+.then((data) => {
     console.log(data);
-} catch (error) {
-    console.log("Error: ", error);
-}
-}
-
-getAllUsers()
-
+})
+.catch((error) => {
+    console.log(error);
+})
