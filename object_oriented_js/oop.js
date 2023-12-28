5f99739a8b044454b6cd81b87ab1557d072d42d4
Shriharsh <shriharshgautam7@gmail.com>
2023-12-28 20:43:37 +0530

    Created constructor function

--- object_oriented_js/oop.js
+++ object_oriented_js/oop.js
@@ -1,13 +1,31 @@
-const user = {
-    username: "Shriharsh",
-    loginCount: 8,
-    signedIn: true,
+// const user = {
+//     username: "Shriharsh",
+//     loginCount: 8,
+//     signedIn: true,
 
-    getUserDetails: function(){
-        // console.log(`username: ${this.username}`);
-        console.log(this);
-    }
+//     getUserDetails: function(){
+//         console.log(`username: ${this.username}`);
+//         // console.log(this);
+//     }
+// }
+
+// console.log(user.username);
+// console.log(user.getUserDetails());
+
+
+function User(username, loginCount, isLoggedIn) {
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+    return this;
 }
 
-console.log(user.username);
-console.log(user.getUserDetails());
+const userOne = new User("Shriharsh", 34, true)
+const userTwo = new User("Rohan", 24, false)
+
+
+console.log(userOne);
+console.log(userTwo);
+
+ 
