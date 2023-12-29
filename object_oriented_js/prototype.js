26fb2c3439472e25a9a726dc6e692a3a1df3b5c1
Shriharsh <shriharshgautam7@gmail.com>
2023-12-29 21:04:12 +0530

    Created trueLength method that gives actual length of string

--- object_oriented_js/prototype.js
+++ object_oriented_js/prototype.js
@@ -1,25 +1,64 @@
-// let myName = "Shriharsh   "
 
-// myName.prototype.true
-// console.log(myName.trueLength);
 
-let myHeros = ["thor", "spiderman"]
+// let myHeros = ["thor", "spiderman"]
 
-let heroPower = {
-    thor: "hammer",
-    spiderman: "sling",
-}
+// let heroPower = {
+//     thor: "hammer",
+//     spiderman: "sling",
+// }
 
-Object.prototype.CustomMethod = function(){
-    console.log(`This is a custom function and is present in all objects`);
-}
+// Object.prototype.CustomMethod = function(){
+//     console.log(`This is a custom function and is present in all objects`);
+// }
 
-heroPower.CustomMethod()
-myHeros.CustomMethod()
+// heroPower.CustomMethod()
+// myHeros.CustomMethod()
 
-Array.prototype.AnotherMethod = function(){
-    console.log(`This method was injected in Array prototype`);
-}
+// Array.prototype.AnotherMethod = function(){
+//     console.log(`This method was injected in Array prototype only`);
+// }
 
 // myHeros.AnotherMethod()
-heroPower.AnotherMethod()
+// // heroPower.AnotherMethod() -- This doesn't work
+
+// // inheritance
+
+// //Old syntax
+
+const User = {
+    name: "chai",
+    email: "chai@google.com"
+}
+
+const Teacher = {
+    makeVideo: true
+}
+
+const TeachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: TeachingSupport
+}
+
+Teacher.__proto__ = User
+
+// modern syntax
+Object.setPrototypeOf(TeachingSupport, Teacher)
+
+
+let anotherUsername = "Shriharsh   "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`True length is: ${this.trim().length}`);
+    
+}
+
+anotherUsername.trueLength()
+
+"Coding".trueLength()
+"Coffee".trueLength()
